0b0a1c3e6696bbf4099cd8a8c35515c513127a57
Nanaya Miki <nanayamiki@gmail.com>
2023-06-21 02:37:58 +0000

    implement the feedback

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,3 @@
-// const properties = require("./json/properties.json");
-// const users = require("./json/users.json");
 const pg = require("pg");
 const Pool = pg.Pool;
 const config = {
@@ -117,44 +115,35 @@ const getAllProperties = function (options, limit = 10) {
 		SELECT properties.*, avg(property_reviews.rating) AS average_rating
 		FROM properties
 		JOIN property_reviews ON properties.id = property_id
+		WHERE 1=1
 	`;
 
 	if (options.city) {
 		queryParams.push(`%${options.city}%`);
-		queryString += `WHERE city LIKE $${queryParams.length} `;
+		queryString += `AND city LIKE $${queryParams.length} `;
 	}
 
 	if (options.owner_id) {
 		queryParams.push(options.owner_id);
-		queryString += `WHERE owner_id = $${queryParams.length} `;
+		queryString += `AND owner_id = $${queryParams.length} `;
 	}
 
 	if (options.minimum_price_per_night && options.maximum_price_per_night) {
 		queryParams.push(options.minimum_price_per_night * 100); // Convert to cents
 		queryParams.push(options.maximum_price_per_night * 100); // Convert to cents
-		if (queryParams.length === 2) {
-			queryString += `WHERE cost_per_night >= $${
-				queryParams.length - 1
-			} AND cost_per_night <= $${queryParams.length} `;
-		} else {
-			queryString += `AND cost_per_night >= $${
-				queryParams.length - 1
-			} AND cost_per_night <= $${queryParams.length} `;
-		}
+		queryString += `AND cost_per_night >= $${
+			queryParams.length - 1
+		} AND cost_per_night <= $${queryParams.length} `;
 	}
 
+	queryString += `GROUP BY properties.id\n`;
 	if (options.minimum_rating) {
 		queryParams.push(options.minimum_rating);
-		if (queryParams.length === 1) {
-			queryString += `WHERE avg(property_reviews.rating) >= $${queryParams.length} `;
-		} else {
-			queryString += `AND avg(property_reviews.rating) >= $${queryParams.length} `;
-		}
+		queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
 	}
 
 	queryParams.push(limit);
 	queryString += `
-		GROUP BY properties.id
 		ORDER BY cost_per_night
 		LIMIT $${queryParams.length};
 	`;
@@ -174,7 +163,7 @@ const addProperty = function (property) {
 		property.owner_id,
 		property.cover_photo_url,
 		property.thumbnail_photo_url,
-		property.cost_per_night,
+		property.cost_per_night * 100,
 		property.parking_spaces,
 		property.number_of_bathrooms,
 		property.number_of_bedrooms,
